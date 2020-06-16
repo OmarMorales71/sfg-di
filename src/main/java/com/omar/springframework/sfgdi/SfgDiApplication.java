13a60b0507bdd2fab9d5c9a4119436122f83c2c3
Omar Morales Ortega <morales.ortega.omar@gmail.com>
2020-06-15 20:17:38 -0500

    Dependency Injection with Spring

--- src/main/java/com/omar/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/com/omar/springframework/sfgdi/SfgDiApplication.java
@@ -1,9 +1,15 @@
 package com.omar.springframework.sfgdi;
 
+import com.omar.springframework.sfgdi.controllers.ConstructorInjectedController;
 import com.omar.springframework.sfgdi.controllers.MyController;
+import com.omar.springframework.sfgdi.controllers.PropertyInjectedController;
+import com.omar.springframework.sfgdi.controllers.SetterInjectedController;
+import com.sun.scenario.effect.impl.sw.java.JSWBlend_SRC_OUTPeer;
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.w3c.dom.ls.LSOutput;
 
 @SpringBootApplication
 public class SfgDiApplication {
@@ -17,6 +23,19 @@ public class SfgDiApplication {
 		String greeting = myController.sayHello();
 
 		System.out.println(greeting);
-	}
+
+		PropertyInjectedController propertyInjectedController = applicationContext.getBean("propertyInjectedController", PropertyInjectedController.class);
+
+		System.out.println("Property: "+propertyInjectedController.getGreeting());
+
+		SetterInjectedController setterInjectedController = applicationContext.getBean("setterInjectedController", SetterInjectedController.class);
+
+		System.out.println("Setter: "+setterInjectedController.getGreeting());
+
+		ConstructorInjectedController constructorInjectedController = applicationContext.getBean("constructorInjectedController", ConstructorInjectedController.class);
+
+		System.out.println("Constructor: "+constructorInjectedController.getGreeting());
+
+}
 
 }
--- src/main/java/com/omar/springframework/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/com/omar/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,11 +1,15 @@
 package com.omar.springframework.sfgdi.controllers;
 
 import com.omar.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
 
     private GreetingService greetingService;
 
+    //@Autowired autowired is optional for constructors
     public  ConstructorInjectedController(GreetingService greetingService){
         this.greetingService=greetingService;
     }
