a1d1cfe172867c1153a53bbed6baccfd02f2f571
Omar Morales Ortega <morales.ortega.omar@gmail.com>
2020-06-29 17:24:11 -0500

    Spring Environment Properties

--- src/main/java/com/omar/springframework/sfgdi/config/PropertyConfig.java
+++ src/main/java/com/omar/springframework/sfgdi/config/PropertyConfig.java
@@ -1,15 +1,20 @@
 package com.omar.springframework.sfgdi.config;
 
 import com.omar.springframework.sfgdi.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")//classpath del archivo donde estan nuestras nuevas propiedades
 public class PropertyConfig {
+
+    @Autowired
+    Environment env;
 
     @Value("${com.omar.username}")//se asigna el valor de la propiedad com.omar.username
     String user;
@@ -21,9 +26,9 @@ public class PropertyConfig {
     String url;
 
     @Bean
-    public FakeDataSource fakeDatsaSource(){
+    public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(env.getProperty("USERNAME"));//Obtener una propiedad o variable de entorno del sistema operativo
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
